c743ac5f60ee2ade1481874184e551061588fbed
jaju <78911247+KR-jaju@users.noreply.github.com>
2024-04-04 13:44:31 +0000

    Feat: Allocator::free method

--- include/Allocator.hpp
+++ include/Allocator.hpp
@@ -3,18 +3,39 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <bitset>
 
 template <std::size_t l, std::size_t u>
 class Allocator {
 public:
-	Allocator(void *pool): pool((unsigned char *)pool), free_tree(2) {}
+	Allocator(void *pool): pool((unsigned char *)pool), free_tree(2), allocated(0) {}
 	void	*allocate(std::size_t exp) {
 		return (fetchBlock(1, exp));
 	}
-	void	free(void *ptr);
+	void	free(void *ptr) {
+		std::size_t	idx = this->fetchAllocatedBlock(static_cast<unsigned char *>(ptr));
+		std::size_t	parent;
+		std::size_t	buddy;
+
+		if (idx == 0)
+			throw "Unable to free given pointer";
+		this->allocated &= ~(1u << idx); // 할당된 블럭 해제
+		this->free_tree |= (1u << idx); // 할당될 수 있는 자원으로 변경
+		while (idx > 1) {
+			parent = idx >> 1;
+			buddy = idx ^ 1u;
+			if (!(this->free_tree & (1u << buddy))) // 버디랑 합칠 수 없음
+				break ;
+			this->free_tree |= (1u << parent); // 부모가 할당가능해지고
+			this->free_tree &= (~(1u << idx)) & (~(1u << buddy)); // 나와 buddy는 불가능해짐
+			std::cout << idx << ", " << buddy << " merged!" << std::endl;
+			idx = parent;
+		}
+	}
 private:
 	unsigned char	*pool;
 	unsigned int	free_tree;
+	unsigned int	allocated;
 
 	void	*fetchBlock(std::size_t idx, std::size_t exp) {
 		int const	leaf_flag = this->free_tree & (1 << idx);
@@ -28,6 +49,7 @@ private:
 			if (!leaf_flag)
 				return (nullptr);
 			this->free_tree &= ~leaf_flag; // 할당했으므로 free_tree에서 제거
+			this->allocated |= leaf_flag; // 할당했으므로 allocated에 추가
 			return (this->pool + (1 << exp) * (idx - begin));
 		}
 		this->free_tree &= ~leaf_flag; // 현재 블럭이 free하지 않음
@@ -38,17 +60,23 @@ private:
 			return (ret);
 		return (ret = fetchBlock(idx * 2 + 1, exp)); // 왼쪽 도전
 	}
+	std::size_t	fetchAllocatedBlock(unsigned char *ptr) {
+		std::size_t	end = 2 << (u - l); // 마지막
+		std::size_t	idx = 1;
+		unsigned char	*base = this->pool;
+		std::size_t		half = (1 << u >> 1);
+
+		while (idx < end) {
+			if (this->allocated & (1 << idx)) // 할당됨
+				return (idx);
+			if (ptr < base + half) { // 오른쪽
+				idx = idx * 2;
+			} else {
+				idx = idx * 2 + 1;
+				base += half;
+			}
+			half >>= 1;
+		}
+		return (0);
+	}
 };
-
-/*
-leaf인 경우
-1. 크기가 맞음 -> return (addr)
-2. 크기가 안 맞음 -> 분할하고 재귀
-leaf아닌 경우
-1. 크기가 맞음 -> return null
-2. 크기가 안 맞음 -> 재귀
-
-1, 0
-0, 0
-
-*/
--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@ int	main(void) {
 	char	pool[256];
 	Allocator<2, 8>	a(pool);
 
-	void	*ptr1 = a.allocate(32);
-	void	*ptr2 = a.allocate(32);
-	void	*ptr3 = a.allocate(64);
+	void	*ptr1 = a.allocate(5);
+	void	*ptr2 = a.allocate(5);
+	a.free(ptr1);
+	a.free(ptr2);
+	void	*ptr3 = a.allocate(6);
 
 	std::cout << ptr1 << std::endl;
 	std::cout << ptr2 << std::endl;

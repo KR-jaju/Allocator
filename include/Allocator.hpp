1675ff3d7d77578b2b9aa1d53fe7810aea10fffd
jaju <78911247+KR-jaju@users.noreply.github.com>
2024-04-04 12:05:22 +0000

    Feat: fetchBlock complete

--- include/Allocator.hpp
+++ include/Allocator.hpp
@@ -7,48 +7,36 @@
 template <std::size_t l, std::size_t u>
 class Allocator {
 public:
-	Allocator(void *pool) {
-		free_tree = 2;
-	}
-	void	*allocate(std::size_t size) {
-		return (fetchBlock(1, size));
+	Allocator(void *pool): pool((unsigned char *)pool), free_tree(2) {}
+	void	*allocate(std::size_t exp) {
+		return (fetchBlock(1, exp));
 	}
 	void	free(void *ptr);
 private:
 	unsigned char	*pool;
 	unsigned int	free_tree;
 
-	// void	*fetchBlock(std::size_t idx, std::size_t exp) {
-	// 	int const	leaf_flag = this->free_tree & (1 << idx);
-	// 	void	*ret;
-
-	// 	if (exp < l) // 이제는 더 이상 물러날 곳이 없다
-	// 		return (nullptr);
-	// 	if (1 << (exp - l) <= idx && idx < (2 << (exp - l))) // 레벨이 맞음
-	// 		return (leaf_flag ? this->pool + (1 << exp) * (idx - (1 << (exp - l))) : nullptr); // 할당 불가능하면 nullptr
-	// 	this->free_tree &= ~leaf_flag; // 현재 블럭이 free하지 않음
-	// 	this->free_tree |= (leaf_flag << idx) | (leaf_flag << idx << 1); // 현재 블럭이 free했다면 자식이 free하게 분할됨
-	// 	if (ret = fetchBlock(idx * 2, exp) != nullptr) //오른쪽 도전
-	// 		return (ret);
-	// 	return (ret = fetchBlock(idx * 2 + 1, exp)); // 왼쪽 도전
-	// }
-	void	*fetchBlock(std::size_t idx, std::size_t size) {
+	void	*fetchBlock(std::size_t idx, std::size_t exp) {
 		int const	leaf_flag = this->free_tree & (1 << idx);
+		std::size_t const	begin = 1 << (u - exp);
+		std::size_t const	end = 2 << (u - exp);
 		void	*ret;
 
-		if (size >> l == 0) // 이제는 더 이상 물러날 곳이 없다
+		if (idx >= end)
 			return (nullptr);
-		if ((size >> l) <= idx && idx < (size >> l << 1)) { // 레벨이 맞음
+		if (begin <= idx) { // 레벨이 맞음
 			if (!leaf_flag)
 				return (nullptr);
-			this->free_tree &= leaf_flag;
-			return (this->pool + size * (idx - (size >> l)));
+			this->free_tree &= ~leaf_flag; // 할당했으므로 free_tree에서 제거
+			return (this->pool + (1 << exp) * (idx - begin));
 		}
 		this->free_tree &= ~leaf_flag; // 현재 블럭이 free하지 않음
 		this->free_tree |= (leaf_flag << idx) | (leaf_flag << idx << 1); // 현재 블럭이 free했다면 자식이 free하게 분할됨
-		if ((ret = fetchBlock(idx * 2, size)) != nullptr) //오른쪽 도전
+		if (leaf_flag)
+			std::cout << idx << ": split!\n";
+		if ((ret = fetchBlock(idx * 2, exp)) != nullptr) //오른쪽 도전
 			return (ret);
-		return (fetchBlock(idx * 2 + 1, size)); // 왼쪽 도전
+		return (ret = fetchBlock(idx * 2 + 1, exp)); // 왼쪽 도전
 	}
 };
 
